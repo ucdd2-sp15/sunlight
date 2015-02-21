f4cf85b4457015e25b5d1007862b98cc82e729ff
Tom Yeh <doubleshow@gmail.com>
2015-02-21 14:26:18 -0700

    build

---
+++ build/js/app.js
@@ -0,0 +1,9 @@
+$( "#legislators" ).on( "click", function( event ) {
+    legislators.load()
+})
+
+$( "#districts" ).on( "click", function( event ) {
+    districts.load()
+})
+
+// TODO: add loaders for more features
