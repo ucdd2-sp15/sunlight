d9eb93a7eb7e1a4f8bd5897451900bd2f317d6d7
Domenic Murtari <domenic.murtari@gmail.com>
2015-02-23 16:38:30 -0700

    Basic district with zipcodes working

--- contents/districts/index.js
+++ contents/districts/index.js
@@ -2,11 +2,24 @@ var districts = {
 
     searchByZipcode: function(zipcode) {
 
-
         // ref: https://sunlightlabs.github.io/congress/districts.html
 
-        $("#list").html("TODO")
+        var zipcode = zipcode || '80301'
 
+        $.get("https://congress.api.sunlightfoundation.com/districts/locate?zip=" + zipcode, apikey, function(data) {
+
+            console.log('got' + data)
+            if (data.results){
+
+                $.get("/sunlight/districts/list.jade", function(template) {
+                    var html = jade.render(template, {
+                        data: data
+                    })
+                    console.log(html)
+                    $("#list").html(html)
+                })
+            }
+        })
     },
 
     load: function() {
