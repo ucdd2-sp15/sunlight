e4dae7fe3ebfbf4f18727fa76e8e9c2d28a54963
Michael <mike2457@gmail.com>
2015-02-23 17:37:57 -0700

    Adding floor_updates

--- build/floor_updates/index.js
+++ build/floor_updates/index.js
@@ -1,4 +1,4 @@
-var legislators = {
+var floor_updates = {
 
     searchByChamber: function(chamber) {
 
@@ -25,43 +25,6 @@ var legislators = {
         })
 
     },
-
-    searchByName: function(name) {
-
-        // search legistalors by name
-        // ref: https://sunlightlabs.github.io/congress/legislators.html
-
-        $.get("https://congress.api.sunlightfoundation.com/legislators?query=" + name, apikey, function(data) {
-
-            $.get("/sunlight/legislators/list.jade", function(template) {
-                var html = jade.render(template, {
-                    data: data
-                })
-                $("#list").html(html)
-            })
-
-        })
-
-    },
-
-
-    searchByChamber: function(chamber) {
-
-        // search legistalors by chamber 
-        // ref: https://sunlightlabs.github.io/congress/legislators.html
-
-        $.get("https://congress.api.sunlightfoundation.com/legislators?chamber=" + chamber, apikey, function(data) {
-
-            $.get("/sunlight/legislators/list.jade", function(template) {
-                var html = jade.render(template, {
-                    data: data
-                })
-                $("#list").html(html)
-            })
-
-        })
-
-    },    
 
     load: function() {
 
--- contents/floor_updates/index.js
+++ contents/floor_updates/index.js
@@ -1,4 +1,4 @@
-var legislators = {
+var floor_updates = {
 
     searchByChamber: function(chamber) {
 
@@ -26,46 +26,9 @@ var legislators = {
 
     },
 
-    searchByName: function(name) {
-
-        // search legistalors by name
-        // ref: https://sunlightlabs.github.io/congress/legislators.html
-
-        $.get("https://congress.api.sunlightfoundation.com/legislators?query=" + name, apikey, function(data) {
-
-            $.get("/sunlight/legislators/list.jade", function(template) {
-                var html = jade.render(template, {
-                    data: data
-                })
-                $("#list").html(html)
-            })
-
-        })
-
-    },
-
-
-    searchByChamber: function(chamber) {
-
-        // search legistalors by chamber 
-        // ref: https://sunlightlabs.github.io/congress/legislators.html
-
-        $.get("https://congress.api.sunlightfoundation.com/legislators?chamber=" + chamber, apikey, function(data) {
-
-            $.get("/sunlight/legislators/list.jade", function(template) {
-                var html = jade.render(template, {
-                    data: data
-                })
-                $("#list").html(html)
-            })
-
-        })
-
-    },    
-
     load: function() {
 
-        $.get("/sunlight/legislators/ui.jade", function(template) {
+        $.get("/sunlight/floor_updates/ui.jade", function(template) {
             var html = jade.render(template)
             $("#ui").html(html)
         })
